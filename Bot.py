fc5c0beed9dc3f7e46d1f8767c318d401b9b57ad
VladislavBel <vladbel1999@gmail.com>
2020-09-16 22:24:58 +0300

    Add new member greeting

--- Bot.py
+++ Bot.py
@@ -13,7 +13,6 @@ token = read_token()
 client = discord.Client()
 
 messages = joined = 0
-id = client.get_guild(701085066687938621)
 
 
 async def update_status():
@@ -29,10 +28,10 @@ async def update_status():
             messages = 0
             joined = 0
 
-            await asyncio.sleep(5)
+            await asyncio.sleep(3600)
         except Exception as e:
             print(e)
-            await asyncio.sleep(5)
+            await asyncio.sleep(3600)
 
 
 
@@ -40,9 +39,9 @@ async def update_status():
 async def on_member_join(member):
     global joined
     joined += 1
-    for channel in member.server.channels:
-        if str(channel) == 'general':
-            await client.send_message(f"""Hello fellow degenerate {member.mention}""")
+    guild_id = client.get_guild(701085066687938621)
+    welcomes = guild_id.get_channel(701085066687938625)
+    await welcomes.send(f"""Hello fellow degenerate {member.mention}""")
 
 
 @client.event
@@ -50,12 +49,23 @@ async def on_message(message):
 
     global messages
     messages += 1
+    guild_id = client.get_guild(701085066687938621)
 
-    if message.content.find('!hello') != -1:
-        await message.channel.send('Howdy')
+    if message.content.find('Akagi hello') != -1:
+        await message.channel.send('Ah skikahhhhh.... I was waiting for you')
 
-    elif message.content == ('!users'):
-        await message.channel.send(f"""Num of members {id.member_count}""")
+    elif message.content == ('Akagi users'):
+
+        await message.channel.send(f"""Num of members {guild_id.member_count}""")
+
+    elif message.content == ('Akagi members status'):
+
+        members = set(client.get_all_members())
+        mem_status = {'online': 0, 'idle': 0, 'do not disturb': 0, 'offline': 0}
+        
+        for member in members:
+            mem_status[str(member.status)] += 1
+        await message.channel.send(f"""Online: {mem_status['online']},\nIdle/DND: {mem_status['idle']}/{mem_status['do not disturb']},\nOffline: {mem_status['offline']}.""")
 
 client.loop.create_task(update_status())
 client.run(token)
